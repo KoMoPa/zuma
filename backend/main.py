f47d5eae45389a28145b38a57787184b3afbcb1b
paulina koch <pjassakoch@gmail.com>
2025-08-27 15:14:57 -0400

    Set up firebase auth, database and cloudstorage but still trying to figure out how to set up the Admin SDK for this app

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,22 @@
+
 from fastapi import FastAPI
+import firebase_admin
+from firebase_admin import firestore, auth 
+
+firebase_admin.initialize_app()
+
+db = firestore.client()
+auth_sdk = auth 
+
 
 app = FastAPI()
 
-@app.get("/")
-def root():
-    return {"message": "Hello ??"}
+
+@app.get("/hello-firestore")
+async def hello_firestore():
+    doc_ref = db.collection('settings').document('general')
+    doc = doc_ref.get()
+    if doc.exists:
+        return {"message": "Settings found!", "data": doc.to_dict()}
+    else:
+        return {"message": "No settings document found."}
